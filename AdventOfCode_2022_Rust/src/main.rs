00270d6c29856cbab8599f5ed2f9ff614271a515
Matthew Remmel <matt.remmel@gmail.com>
2022-12-08 13:55:56 -0500

    Complete day two

--- AdventOfCode_2022_Rust/src/main.rs
+++ AdventOfCode_2022_Rust/src/main.rs
@@ -1,9 +1,11 @@
 use crate::common::Challenge;
 use day1::Day1;
+use day2::Day2;
 use std::fs;
 
 mod common;
 mod day1;
+mod day2;
 
 fn main() {
     // Day 1
@@ -11,4 +13,8 @@ fn main() {
     println!("Day 1 - Part 1: {}", Day1::part1(&day1_input));
     println!("Day 1 - Part 2: {}", Day1::part2(&day1_input));
 
+    // Day 2
+    let day2_input = fs::read_to_string("./inputs/day2.txt").unwrap();
+    println!("Day 2 - Part 1: {}", Day2::part1(&day2_input));
+    println!("Day 2 - Part 2: {}", Day2::part2(&day2_input));
 }
